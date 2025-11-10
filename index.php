8be73b2c537843cdd78a26160be87adfd2644114
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2025-11-11 00:02:51 +0100

    feat: add router class and home controller

---
+++ index.php
@@ -0,0 +1,12 @@
+layout strony
+
+<?php 
+
+require_once './core/Router.php';
+require_once './app/Controllers/HomeController.php';
+
+$router = new Router();
+
+$router->get('/', ['HomeController', 'index']);
+
+$router->resolve();
